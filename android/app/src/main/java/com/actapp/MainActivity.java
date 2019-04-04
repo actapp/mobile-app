fe16e8439ac51e465ca3192378f7fb3674774cd2
Drew Dennis <dennisdrew@gmail.com>
2019-04-04 07:53:52 -0500

    Fix Android app name

--- android/app/src/main/java/com/actapp/MainActivity.java
+++ android/app/src/main/java/com/actapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "ActApp";
+        return "MySharePal";
     }
 }
