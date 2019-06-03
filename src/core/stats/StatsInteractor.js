6a10dfb327a981fc1cc483a41386661f10236332
Drew Dennis <dennisdrew@gmail.com>
2019-06-02 22:00:46 -0500

    Working on incremental stats

--- src/core/stats/StatsInteractor.js
+++ src/core/stats/StatsInteractor.js
@@ -1,5 +1,59 @@
-import { StatsService } from '../../AppDI'
+import StatsService from './StatsService'
 
-export async function getStats(uid, mid, role) {
-    return await StatsService.getStats(uid, mid, role)
+/**
+ * Get the stats of:
+ * - this user
+ * - this user's ministry
+ * - the global community
+ * 
+ * If no ministry ID, there will be no ministry stats
+ */
+export async function getStats(uid, mid) {
+    let userStats = await StatsService.getStatsForUser(uid)
+    if(userStats == null) {
+        userStats = createDomainSpecificStats()
+        await StatsService.updateStatsForUser(uid, userStats)
+    }
+    
+    let ministryStats = null
+    if(mid != null) {
+        ministryStats = await StatsService.getStatsForMinistry(mid)
+        if(ministryStats == null) {
+            ministryStats = createDomainSpecificStats()
+            await StatsService.updateStatsForMinistry(mid, stats)
+        }
+    }
+
+    // Global stats should NOT be null
+    const globalStats = await StatsService.getStatsForGlobal()
+
+    return {
+        user: userStats,
+        ministry: ministryStats,
+        global: globalStats
+    }
+}
+
+/**
+ * Increment total # of spiritual convos for:
+ * - this user
+ * - this user's ministry
+ * - the global community
+ * 
+ * If ministry ID is null, ministry will be ignored
+ */
+export async function incrementConversations(uid, mid) {
+    const stats = await getStats(uid, mid)
+
+    const newUserStats = {
+        convos: stats.user.convos + 1,
+        // conversions: stats.
+    }
+}
+
+function createDomainSpecificStats() {
+    return {
+        convos: 0,
+        conversions: 0
+    }
 }
--- src/core/stats/StatsService.js
+++ src/core/stats/StatsService.js
@@ -1,5 +1,42 @@
-export default class StatsService {
-    static getStats = async (uid, mid, role) => {
+import { getDocAndRefs } from '../util/DocManagement'
 
+const USERS_COLLECTION = 'users'
+const MINISTRY_COLLECTION = 'ministries'
+
+const GLOBAL_COLLECTION = 'global'
+const GLOBAL_DOCUMENT = 'global'
+
+export default class StatsService {
+    static getStatsForUser = async uid => {
+        const { doc } = await getDocAndRefs(USERS_COLLECTION, uid)
+        return doc.data().stats
+    }
+
+    static updateStatsForUser =  async (uid, stats) => {
+        const { docRef } = await getDocAndRefs(USERS_COLLECTION, uid)
+        await docRef.update({ stats })
+        return stats
+    }
+
+    static getStatsForMinistry = async mid => {
+        const { doc } = await getDocAndRefs(MINISTRY_COLLECTION, mid)
+        return doc.data().stats
+    }
+
+    static updateStatsForMinistry = async (mid, stats) => {
+        const { docRef } = await getDocAndRefs(MINISTRY_COLLECTION, uid)
+        await docRef.update({ stats })
+        return stats
+    }
+
+    static getStatsForGlobal = async () => {
+        const { doc } = await getDocAndRefs(GLOBAL_COLLECTION, GLOBAL_DOCUMENT)
+        return doc.data().stats
+    }
+
+    static updateStatsForGlobal = async stats => {
+        const { docRef } = await getDocAndRefs(GLOBAL_COLLECTION, GLOBAL_DOCUMENT)
+        await docRef.update({ stats })
+        return stats
     }
 }
